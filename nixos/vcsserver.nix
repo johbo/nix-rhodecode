e43fc0747ebeacb825e5ee7981785eff5008c456
Johannes Bornhold <johannes@bornhold.name>
2017-10-31 21:40:08 +0100

    Fix vcsserver script to call gunicorn

--- nixos/vcsserver.nix
+++ nixos/vcsserver.nix
@@ -66,7 +66,7 @@ in {
       after = [ "network.target" ];
       path = [ cfg.package ];
       script = ''
-        exec pserve ${configFile}
+        exec gunicorn --paste ${configFile}
       '';
       serviceConfig = {
         User = "vcsserver";
