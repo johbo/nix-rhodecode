96a70462b1275b43076aeb63bbcedc08be68824d
Johannes Bornhold <johannes@bornhold.name>
2017-11-02 23:18:19 +0100

    Use paste's inheritance mechanism to provide secrets

--- nixos/test-machine.nix
+++ nixos/test-machine.nix
@@ -6,11 +6,35 @@
     ./vcsserver.nix
   ];
 
-  deployment.keys = {
+  deployment.keys = let
+    cfg = config.services.rhodecode-enterprise;
+  in {
     enterprise-initial-password = {
       text = "secret";
     };
+    enterprise-secret-config = {
+      text = ''
+        [server:main]
+        use = config:${cfg.baseConfigFile}
+
+        [app:main]
+        use = config:${cfg.baseConfigFile}
+        sqlalchemy.db1.url = sqlite:///${cfg.dataDir}/rhodecode.db?timeout=30
+
+        # TODO: Set the following
+        # rhodecode.encrypted_values.secret =
+        app_instance_uuid = rc-pre-prod-test-johannes
+        channelstream.secret = secret
+        beaker.session.secret = peoen32w9x8en
+      '';
+      user = "enterprise";
+      group = "enterprise";
+    };
   };
+
+  users.groups.keys.members = [
+    "enterprise"
+  ];
 
   networking.firewall.allowedTCPPorts = [
     5000
@@ -18,6 +42,7 @@
 
   services.rhodecode-enterprise = {
     enable = true;
+    secretConfigFile = "/run/keys/enterprise-secret-config";
   };
 
   services.rhodecode-vcsserver = {
